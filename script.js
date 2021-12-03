ad1fd86ffe77c449ed5d31ad6b08af9fad920881
Oliver Hall <olly.hall97@gmail.com>
2021-12-03 17:03:23 +0000

    Add button and planning

---
+++ script.js
@@ -0,0 +1,15 @@
+"strict";
+
+const addTask = document.querySelector("add");
+
+// load tasks from browser and display
+
+addTask.addEventListener("click", function () {
+  // open text input
+  // done button
+  // // output new task to list
+  // // save tasks to browser
+  // cancel button
+});
+
+// archive of completed tasks with button to view, option to delete
