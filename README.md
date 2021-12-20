066d4f18c2d365d10d736374e95e915e05caa9e3
Oliver Hall <olly.hall97@gmail.com>
2021-12-20 15:48:57 +0000

    Tasks can be set as completed or not. Local storage utilised.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # To Do List Web App
 
 A vanilla JavaScript application which will allow users to add to do tasks to a list, delete tasks, mark tasks as done, review completed tasks and sort tasks by date added.  
-Currently a work in progress, the ability to add tasks has been implemented.
+Currently a work in progress. So far you can:
+
+- Add tasks to your to do list using the + button.
+- Close the page without losing your tasks when you reopen (local storage stores tasks).
+- Mark tasks as completed or not with the circle button on each task.
+- Clear all tasks from the current session & local storage with the 'Clear All Tasks' button.
--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const formTaskTitle = document.querySelector('.form__task-title');
 const formTaskDescription = document.querySelector('.form__task-description');
 const form = document.querySelector('.modal__form');
-const taskList = document.querySelector('.task-row');
+const taskList = document.querySelector('.tasks');
+const btnClear = document.querySelector('.btn__clear-tasks');
 
 // load tasks from browser and display
 
@@ -18,8 +19,19 @@ const taskList = document.querySelector('.task-row');
 
 // archive of completed tasks with button to view, option to delete
 
-/// Main App
+// Task class
+class Task {
+  date = new Intl.DateTimeFormat(navigator.language).format(new Date());
+  complete = false;
+  key = String(Math.floor(Date.now() / 1000)).slice(3);
 
+  constructor(title, description) {
+    this.title = title;
+    this.description = description;
+  }
+}
+
+// Main App
 class App {
   _tasks = [];
 
@@ -31,13 +43,25 @@ class App {
     // Close modal when user clicks off modal window
     overlay.addEventListener('click', this._closeModal);
     // Close on esc key press
-    document.addEventListener('keydown', function (e) {
-      if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-        this._closeModal();
-      }
-    });
+    document.addEventListener(
+      'keydown',
+      function (e) {
+        if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+          this._closeModal();
+        }
+      }.bind(this)
+    );
 
     form.addEventListener('submit', this._submitForm.bind(this));
+
+    // Toggle the completed mark on tasks.
+    taskList.addEventListener('click', this._toggleComplete.bind(this));
+
+    // Load and display tasks from local storage
+    this._getTasksFromLocal();
+
+    // Clear all tasks from _task array and local storage
+    btnClear.addEventListener('click', this._clear);
   }
 
   // Function to show the modal input window
@@ -53,29 +77,72 @@ class App {
     overlay.classList.add('hidden');
   }
 
-  // Take modal form input and places it in tasks array
-  _submitForm(e) {
-    e.preventDefault();
-    const task = {
-      title: formTaskTitle.value,
-      description: formTaskDescription.value,
-      timeDate: new Date(),
-      complete: false,
-    };
-    this._tasks.push(task);
-
+  _renderTask(task) {
     const html = `
     <div class="task-row">
-      <div class="task-title">Task - ${task.title}</div>
+      <div class="task-title" id="${task.key}">
+        <span class="material-icons md-36 tertiary" id="completed">${
+          task.complete ? 'check_' : ''
+        }circle</span>
+        <span>Task - ${task.title}</span>
+        <span style="font-size: 14pt">${task.date}</span>
+      </div>
       <div class="task-description">
         ${task.description}
       </div>
     </div>
     `;
 
-    taskList.insertAdjacentHTML('beforebegin', html);
+    taskList.insertAdjacentHTML('afterbegin', html);
+  }
 
+  _saveToLocal() {
+    localStorage.setItem('tasks', JSON.stringify(this._tasks));
+  }
+
+  // Renders all tasks from local storage
+  _getTasksFromLocal() {
+    const data = JSON.parse(localStorage.getItem('tasks'));
+
+    if (!data) return;
+
+    this._tasks = data;
+    this._tasks.forEach(task => this._renderTask(task));
+  }
+
+  // Take modal form input and places it in tasks array
+  _submitForm(e) {
+    e.preventDefault();
+    const task = new Task(formTaskTitle.value, formTaskDescription.value);
+    this._tasks.push(task);
+    this._saveToLocal();
+    this._renderTask(task);
     this._closeModal();
+    formTaskTitle.value = formTaskDescription.value = '';
+  }
+
+  _toggleComplete(e) {
+    // Select the closest element with the id value 'completed'
+    const tickCircle = e.target.closest('#completed');
+    if (!tickCircle) return;
+    // Get the id of the parent element. This is the key assigned to each task object.
+    const taskKey = tickCircle.parentElement.id;
+    // Find the task in _tasks array with that key value.
+    const task = this._tasks.find(task => task.key === taskKey);
+    // Toggle the completed value.
+    task.complete = !task.complete;
+    // Re-render the task list to show the changed completed mark.
+    taskList.innerHTML = '';
+    this._tasks.forEach(task => this._renderTask(task));
+    // Save the changes to local storage.
+    this._saveToLocal();
+  }
+
+  // Clear all tasks from _tasks array and local storage
+  _clear() {
+    this._tasks = [];
+    localStorage.removeItem('tasks');
+    taskList.innerHTML = '';
   }
 }
 
